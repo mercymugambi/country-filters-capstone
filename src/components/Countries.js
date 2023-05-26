e30a88b746e57565e97aa3e21ac8747bd836f314
mercymugambi <mercymugambi2016@gmail.com>
2023-05-27 00:45:27 +0300

    Update readme

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getCountries } from '../redux/region/CountrySlice';
@@ -8,19 +8,32 @@ import { RightIcon } from './icons';
 const Countries = () => {
   const dispatch = useDispatch();
   const { countries, loading } = useSelector((state) => state.countries);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     dispatch(getCountries());
   }, [dispatch]);
 
   const countryMapping = {};
-  countries.forEach((country) => {
-    countryMapping[country.tld] = country;
-  });
+  if (countries) {
+    countries.forEach((country) => {
+      countryMapping[country.tld] = country;
+    });
+  }
 
   const handleCountryClick = (tld) => {
     console.log(`Clicked country with TLD: ${tld}`);
   };
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredCountries = countries
+    ? Object.values(countryMapping).filter((country) =>
+      // eslint-disable-next-line implicit-arrow-linebreak
+      country.name.common.toLowerCase().includes(searchQuery.toLowerCase()))
+    : [];
 
   return (
     <div>
@@ -28,32 +41,35 @@ const Countries = () => {
         <Header />
       </div>
       <div className="search-container">
-        {' '}
         <p className="search-Title">Search For Country</p>
-        {' '}
-        <input className="search-Input-Area" placeholder="e.g. Andorra" onChange={handleSearch} />
-        {' '}
+        <input
+          className="search-Input-Area"
+          placeholder="e.g. Kenya"
+          value={searchQuery}
+          onChange={handleSearch}
+        />
       </div>
 
       {loading ? (
         <p>Loading...</p>
       ) : (
         <div className="countries-grid">
-          {Object.keys(countryMapping).map((tld) => {
-            const country = countryMapping[tld];
-            return (
-              <div key={country.tld} className="country-card-1">
-                <Link to={`/countries/${country.tld}`} className="country-card" onClick={() => handleCountryClick(country.tld)}>
-                  <span className="righticon"><RightIcon /></span>
-                  <img className="flag-image" src={country.flags.png} alt={country.name.common} />
-                  <div className="popSection">
-                    <p>{country.population}</p>
-                    <h3>{country.name.common}</h3>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
+          {filteredCountries.map((country) => (
+            <div key={country.tld} className="country-card-1">
+              <Link
+                to={`/countries/${country.tld}`}
+                className="country-card"
+                onClick={() => handleCountryClick(country.tld)}
+              >
+                <span className="righticon"><RightIcon /></span>
+                <img className="flag-image" src={country.flags.png} alt={country.name.common} />
+                <div className="popSection">
+                  <p>{country.population}</p>
+                  <h3>{country.name.common}</h3>
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
       )}
     </div>
